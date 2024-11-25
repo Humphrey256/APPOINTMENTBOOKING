d858f0ce4dfa623bbf6a81de06f3654b80ec54ac
Humphrey256 <muronhumfix@gmail.com>
2024-11-25 15:51:23 +0300

    build sucess

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+    root: './public',  // Point Vite to the public folder
     plugins: [react()],
 });
