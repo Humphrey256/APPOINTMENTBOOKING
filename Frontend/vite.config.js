02824ba0e7bf483c6b121d493af01aa6ba6e0996
Humphrey256 <muronhumfix@gmail.com>
2024-11-26 15:55:01 +0300

    commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    root: './src',  // Point Vite to the public folder
+    root: './public',  // Point Vite to the public folder
     plugins: [react()],
 });
